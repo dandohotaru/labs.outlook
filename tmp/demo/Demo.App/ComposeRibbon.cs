c054eec3b910d486df74818353916490548c000d
Dan Dohotaru <dan.dohotaru@gmail.com>
2025-02-17 14:08:41 +0100

    work on improving the startup of the addin

---
+++ tmp/demo/Demo.App/ComposeRibbon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Demo.App
+{
+    public partial class ComposeRibbon
+    {
+        private void ComposeRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
